bea95a908a60c5581921233bc85164657d5f38d5
Naime <hossain.naime@yahoo.com>
2017-12-04 15:38:25 +0600

    logout added in dashboard

--- resources/views/admin/layouts/topnav.blade.php
+++ resources/views/admin/layouts/topnav.blade.php
@@ -58,10 +58,20 @@
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                  <a href="" class="btn btn-default btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+               
+                  <a class="btn btn-default btn-flat" href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();">
+                      Logout
+                  </a>
+
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      {{ csrf_field() }}
+                  </form>
+                                   
                 </div>
               </li>
             </ul>
